a4e2e56fa55ad46fc383b2b6bfde3fa966b3174d
Jordan Klein <jordaniklein03@gmail.com>
2020-08-01 09:23:58 -0400

    Initial Commit

---
+++ Rings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rings
+//
+//  Created by Jordan Klein on 8/1/20.
+//  Copyright © 2020 JordanKlein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RingsTests/RingsTests.swift
@@ -0,0 +1,34 @@
+//
+//  RingsTests.swift
+//  RingsTests
+//
+//  Created by Jordan Klein on 8/1/20.
+//  Copyright © 2020 JordanKlein. All rights reserved.
+//
+
+import XCTest
+@testable import Rings
+
+class RingsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
